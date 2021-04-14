2b67b565a1cc882e836cc9d00018cb3f58ce0474
Chad Niedert <>
2021-04-14 02:04:39 +0000

    created is prime static method

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
 // Link: https://projecteuler.net/problem=10
 
 class Main {
-  public static void main(String[] args) {
-    
-  }
+	public static void main(String[] args) {
+		System.out.println(isPrime(5));
+		System.out.println(isPrime(25));
+	}
+
+	public static boolean isPrime(int num)
+	{
+		for (int i = 2; i < num; i++)
+		{
+			if (num % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
